ebd37da68f245262178f0c4ab51d44914abf6a31
wus22 <121781743+wus22@users.noreply.github.com>
2023-08-15 17:06:35 +0900

    [D1] Title: 중간값 찾기, Time: 102 ms, Memory: 18,692 KB -BaekjoonHub

---
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s);
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[n/2]);
+	}
+}

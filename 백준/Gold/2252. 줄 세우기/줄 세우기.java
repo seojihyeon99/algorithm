ebdec523afecd964400db6c52cfe3f8316bd7606
wus22 <121781743+wus22@users.noreply.github.com>
2023-08-21 19:36:51 +0900

    [Gold III] Title: 줄 세우기, Time: 416 ms, Memory: 50052 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -4,6 +4,8 @@
  * DAG(Directed Acyclic Graph)
  * 정정 대비 간선수 작으므로 -> 인접리스트 이용
  * 
+ * 처음에 boolean[] visited = new boolean[n+1]; 배열 만들어줬는데.. 생각해보니 필요 x
+ * 이미 진입차수가 0이어서 방문했으면 -> 다음번에 인접정점이될경우에는 또 -1해서 -1,-2,...되기때문
  */
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,25 +27,19 @@ public class Main {
 		int m = Integer.parseInt(st.nextToken()); // 키 비교횟수
 		
 		// 인접리스트 생성
-//		List<List<Integer>> list1 = new ArrayList<>();
-//		for(int i=0; i<n+1; i++) {
-//			list.add(new ArrayList<>());
-//		}
 		List<Integer>[] list = new List[n+1];
 		for(int i=0; i<n+1; i++) {
 			list[i] = new ArrayList<>();
 		}
 
-		boolean[] visited = new boolean[n+1]; // 방문여부 저장
 		int[] indegree = new int[n+1]; // 진입차수 저장
 		
 		for(int i=0; i<m; i++) { // 비교횟수만큼 반복
 			st = new StringTokenizer(br.readLine());
-			int prev = Integer.parseInt(st.nextToken()); // 선행되어야하는애
-			int next = Integer.parseInt(st.nextToken()); // 그 다음 방문
+			int prev = Integer.parseInt(st.nextToken()); // 선행되어야하는 애
+			int next = Integer.parseInt(st.nextToken()); // 후에 방문해도되는 애
 			indegree[next]++; // 진입차수 1 증가
-//			list.get(prev).add(next);
-			list[prev].add(next);
+			list[prev].add(next); // 인접리스트에 넣어줌
 		}
 				
 		// 시작점들을 큐에 넣어줌
@@ -51,36 +47,22 @@ public class Main {
 
 		// 진입차수가 0인애들 모두 큐에 넣음
 		for(int i=1; i<=n; i++) {
-			if(indegree[i] == 0 && !visited[i]) {
-				queue.offer(i);
-				visited[i] = true;
+			if(indegree[i] == 0) { // 진입차수가 0이면
+				queue.offer(i); // 큐에 넣고
 			}
 		}
 		
 		// 큐가 빌때까지 반복
 		while(!queue.isEmpty()) {			
-			int size = queue.size();
-			for(int i=0; i<size; i++) {
-				int cur = queue.poll();
-				sb.append(cur + " ");
+			int cur = queue.poll(); // 큐에서 하나 꺼내서
+			sb.append(cur + " "); // 출력
 				
-//				for(int v : list.get(cur)) {
-				for(int v : list[cur]) {
-					indegree[v]--;
-					if(indegree[v] == 0 && !visited[v]) {
-						queue.offer(v);
-						visited[v] = true;
-					}
-				}				
-			}
-			
-//			// 진입차수가 0인애들 모두 큐에 넣음
-//			for(int i=1; i<=n; i++) {
-//				if(indegree[i] == 0 && !visited[i]) {
-//					queue.offer(i);
-//					visited[i] = true;
-//				}
-//			}
+			for(int v : list[cur]) { // 인접한 정점
+				indegree[v]--; // 진입차수 1 감소
+				if(indegree[v] == 0) { // 인접한 정점의 진입차수가 0이되었다면
+					queue.offer(v); // 큐에 넣어줌
+				}
+			}				
 		}
 		
 		System.out.println(sb.toString());

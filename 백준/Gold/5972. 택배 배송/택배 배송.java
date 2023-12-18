85fbdd0e456b0331139813c39c0106424ef334ed
wus22 <121781743+wus22@users.noreply.github.com>
2023-12-18 21:33:01 +0900

    [Gold V] Title: 택배 배송, Time: 384 ms, Memory: 39996 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -1,84 +1,104 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 /**
- * 최대 간선수(50000*49999/2)대비 문제에서 주어진 간선수(50000)가 훨씬 적으므로 => 인접리스트 사용!
+ * 처음에 pq안쓰고 매 반복마다 배열의 모든 값 탐색했음.. => 시간초과(50000*50000)
+ * PriorityQueue로 구현한 Dijkstra 알고리즘 이용
  */
-class Node{
-	int node;
-	int edge;
-	
-	public Node(int node, int edge) {
-		super();
-		this.node = node;
-		this.edge = edge;
-	}
-	
-	public int getNode() {
-		return node;
-	}
-	
-	public int getEdge() {
-		return edge;
-	}
-}
 
 public class Main {
+	static class Node {
+		int to;
+		int weight;
+		
+		public Node(int to, int weight) {
+			super();
+			this.to = to;
+			this.weight = weight;
+		}
+	}
 	
-	static boolean[] visited; // 해당 헛간 방문 여부
-	static int[] dist; // 해당 헛간까지의 최단 거리
-	static int cnt; // 방문한 헛간의 수
+	static class Vertex implements Comparable<Vertex>{
+		int no; // 해당 정점 번호
+		int weight; // 시작점과 연결했을 때 간선 비용
+		
+		public Vertex(int no, int weight) {
+			super();
+			this.no = no;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Vertex o) {
+			return this.weight-o.weight;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()); // 정점의 수
+		int m = Integer.parseInt(st.nextToken()); // 간선의 수
 		
-		String[] c = br.readLine().split(" ");
-		int n = Integer.parseInt(c[0]); // 헛간의 개수
-		int m = Integer.parseInt(c[1]); // 길의 개수
+		boolean[] visited = new boolean[n+1]; // 해당 정점 방문 여부
+		int[] dist = new int[n+1]; // 시작점 ~ 해당 정점까지의 최소 거리
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[1] = 0; // 정점 1이 시작점
 		
-		visited = new boolean[n+1];
-		dist = new int[n+1];
-		Arrays.fill(dist, Integer.MAX_VALUE); // 정수 최댓값으로 초기화
-		
-		List<Node>[] list = new List[n+1];
+		List<Node>[] adjList = new List[n+1];
 		for(int i=1; i<=n; i++) {
-			list[i] = new ArrayList<>();
+			adjList[i] = new ArrayList<>();
 		}
-		// 현재 길의 정보 입력받기		
+		// 간선 정보 입력받음
 		for(int i=0; i<m; i++) {
-			c = br.readLine().split(" ");
-			int v1 = Integer.parseInt(c[0]); // 시작 정점
-			int v2 = Integer.parseInt(c[1]); // 끝 정점
-			int edge = Integer.parseInt(c[2]); // 간선 가중치
-			
-			// 양방향이므로 두 정점리스트에 모두 추가해줌
-			list[v1].add(new Node(v2, edge));
-			list[v2].add(new Node(v1, edge));
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			// 양방향 그래프이므로, 둘 다 넣어줌
+			adjList[from].add(new Node(to, weight));
+			adjList[to].add(new Node(from, weight));
 		}
 		
-		// 여기서 부터 시뮬레이션 시작!!
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.offer(1); // 시작 정점(1)을 큐에 넣음
-		dist[1] = 0;
-		while(!queue.isEmpty()) {
-			int cur = queue.poll(); // 현재 탐색중인 정점
+		PriorityQueue<Vertex> pq = new PriorityQueue<>();
+		pq.offer(new Vertex(1, 0)); // 시작 정점 pq에 넣음
+		while(!pq.isEmpty()) {
+			// 현재 방문하지 않은 정점들 중, 시작점부터 가장 거리가 최소인 정점 찾음
+			Vertex cur = pq.poll();
+			if(visited[cur.no]) continue; // 이미 방문했다면 -> 다시 pq에서 꺼냄
 			
-			for(int i=0; i<list[cur].size(); i++) { // 이웃하는 정점들 탐색
-				int adj = list[cur].get(i).getNode();
-				if(dist[adj] > dist[cur] + list[cur].get(i).getEdge()) { // 간선 거리 더 짧다면 업데이트
-					dist[adj] = dist[cur] + list[cur].get(i).getEdge();	
-					queue.offer(adj); // 큐에 다시 추가
+			visited[cur.no] = true; // 해당 정점 방문 처리
+			
+			// 해당 정점과 이웃한 정점들 탐색
+			for(Node node : adjList[cur.no]) {
+				if(!visited[node.to] && dist[node.to] > dist[cur.no] + node.weight) {
+					dist[node.to] = dist[cur.no] + node.weight; // 거리 더 짧다면 업데이트
+					pq.offer(new Vertex(node.to, dist[node.to])); // pq에 업데이트된 정점과 거리 넣음
 				}
 			}
 			
+			if(cur.no == n) break; // 도착지라면 break
 		}
 		
 		System.out.println(dist[n]);
+	}
+	
+	static int findMin(int[] dist, boolean[] visited) {
+		int min = Integer.MAX_VALUE;
+		int vertex = -1;
+		for(int i=1; i<dist.length; i++) {
+			if(!visited[i] && min > dist[i]) {
+				min = dist[i];
+				vertex = i;
+			}
+		}
+		return vertex;
 	}
 }

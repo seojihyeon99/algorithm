166695c5025acd33cd80c00b030936f49e79b02c
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-05-19 11:44:08 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,73 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int[][] map;
-	private static int n;
-	private static int m;
-	private static boolean[][] visited;
-
-	private static int[] dx = { -1, 1, 0, 0 }; // x 방향벡터(상하좌우)
-	private static int[] dy = { 0, 0, -1, 1 }; // y 방향벡터(상하좌우)
-
-	// 상하좌우 중 한곳으로 이동 가능한지 여부
-	public static boolean canGo(int r, int c, int idx) {
-		int x = r + dx[idx];
-		int y = c + dy[idx];
-		// 인덱스를 벗어났는지 여부 체크
-		if ((x >= 0 && x < n) && (y >= 0 && y < m)) {
-			// 이미 방문한곳이거나 이동할 수 없는 칸인지 체크
-			if (visited[x][y] == false && map[x][y] != 0)
-				return true;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()); // 세로
+		int m = Integer.parseInt(st.nextToken()); // 가로
+		
+		// 배열 초기화 및 입력받기
+		int[][] map = new int[n][m];
+		for(int r=0; r<n; r++) {
+			String s = br.readLine();
+			for(int c=0; c<m; c++) {
+				map[r][c] = s.charAt(c) - '0';
+			}
 		}
-		return false;
-	}
-
-	public static void bfs(int r, int c) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] { r, c }); //큐에 넣어줌
-		visited[r][c] = true; // 시작위치를 방문처리
-		while (!queue.isEmpty()) {
-			int[] point = queue.poll(); //큐에서 하나 뺌
-			int x = point[0]; // 현재 디큐한 좌표의 x좌표
-			int y = point[1]; // 현재 디큐한 좌표의 y좌표
-			for (int i = 0; i < 4; i++) {
-				// 상하좌우 중 한곳으로 이동 가능하다면
-				if (canGo(x, y, i)) {
-					int nx = x + dx[i]; //x 좌표 이동
-					int ny = y + dy[i]; //y 좌표 이동
-					map[nx][ny] = map[x][y]+1; //새로운 위치값 = 현재 위치값+1로
-					queue.offer(new int[] {nx,ny}); //큐에 넣어줌
-					visited[nx][ny]=true; //현재 좌표 방문처리
+		boolean[][] visited = new boolean[n][m];
+		
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {0,0,1});
+		visited[0][0] = true;
+		
+		// 상하좌우
+		int[] dx = new int[] {-1,1,0,0};
+		int[] dy = new int[] {0,0,-1,1};
+		int cnt = 0;
+		
+		while(!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			
+			// 종료 조건 (도착 지점일 경우)
+			if(cur[0] == n-1 && cur[1] == m-1) {
+				cnt = cur[2];
+				break;
+			}
+			
+			// 동서남북 탐색
+			for(int i=0; i<4; i++) {
+				int curx = cur[0] + dx[i];
+				int cury = cur[1] + dy[i];
+				
+				if(curx>=0 && curx<n && cury>=0 && cury<m && map[curx][cury] == 1 && !visited[curx][cury]) {
+					queue.offer(new int[] {curx, cury, cur[2] + 1});
+					visited[curx][cury] = true;
+					
 				}
 			}
 		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-
-		map = new int[n][m]; // 2차원 배열(미로) 저장
-		visited = new boolean[n][m]; // 2차원 배열(미로)에서 방문여부 체크
-
-		// 2차원 배열(미로) 입력 받기
-		for (int i = 0; i < n; i++) {
-			String s = br.readLine();
-			for (int j = 0; j < m; j++) {
-				map[i][j] = s.charAt(j) - '0';
-			}
-		}
-
-		bfs(0, 0);
-		System.out.println(map[n-1][m-1]); //배열의 맨 오른쪽(m-1) 아래(n-1)값 출력 
+		
+		System.out.println(cnt);
+		
+		
 	}
 }

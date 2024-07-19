955ebd7cd11067effb0f39bf015c73e8ec76c670
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-07-20 02:04:02 +0900

    [Silver III] Title: 퇴사, Time: 80 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
@@ -9,30 +8,28 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine()); // 남은 날짜
-		int[][] arr = new int[2][n+1]; // 상담 일정표
+		int[][] schedule = new int[2][n+1]; // 상담 일정표 (0행: 시간, 1행: 비용)
 		
 		for(int i=1; i<=n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[0][i] = Integer.parseInt(st.nextToken()); // 상담시간
-			arr[1][i] = Integer.parseInt(st.nextToken()); // 상담비용
+			schedule[0][i] = Integer.parseInt(st.nextToken()); // 상담시간
+			schedule[1][i] = Integer.parseInt(st.nextToken()); // 상담비용
 		}
 		
-		int[] dp = new int[n+1]; // dp[i] : i번째날에 얻을 수 있는 최대 수익
-		for(int i=1; i<=n; i++) {
-			dp[i] = dp[i-1];
-			
-			// 상담 기간의 최소 1일 ~ 최대 5일 => 그 전 5개를 탐색
-			for(int j=i; j>=i-5; j--) {
-				if(j < 1) break;
-				
-				// 현재 날짜에 끝나는 상담이 있다면, 최대 수익 업데이트 될 가능성 있음
-				if(j + arr[0][j] - 1 == i) {
-					dp[i] = Math.max(dp[i], dp[j-1] + arr[1][j]);
-				}
+		int[] dp = new int[n+2]; // dp[i]: i일부터 마지막날(n)까지의 최대이익
+		for(int i=n; i>=1; i--) {
+			// 해당 날짜에 상담 진행 o
+			int take = 0;
+			if(i + schedule[0][i] -1 <= n) {
+				take = dp[i + schedule[0][i]] + schedule[1][i];
 			}
+			
+			// 해당 날짜에 상담 진행 x
+			int notTake = dp[i+1];
+			
+			dp[i] = Math.max(take, notTake);
 		}
 		
-		System.out.println(dp[n]);
-		
+		System.out.println(dp[1]);
 	}
 }

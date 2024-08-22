1436a778443da53ddd2d83dac4955470daaed1c2
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-08-22 19:36:34 +0900

    [Gold III] Title: 정상 회담 2, Time: 136 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Gold/1670. 정상 회담 2/정상 회담 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		
+		int[] dp = new int[10001]; // dp[i] : i명일때 완벽한 악수의 수
+		dp[0] = 1;
+		dp[2] = 1;
+		dp[4] = 2;
+		
+		for(int i=6; i<=n; i+=2) { // 홀수일때는 완벽한 악수 불가능!! (한 명 남음)
+			// 한 사람(기준) => 나머지 사람들과 악수
+			for(int j=0; j<=i-2; j+=2) {
+				// 한 사람(기준)과 악수하는 사람의 왼쪽 부분
+				int left = j;
+				
+				// 한 사람(기준)과 악수하는 사람의 오른쪽 부분
+				int right = i-2-j;
+				
+				dp[i] += ((long)dp[left]*dp[right]) % 987654321;
+				dp[i] = dp[i] % 987654321;
+			}
+		}
+		
+		System.out.println(dp[n]);
+		
+	}
+}

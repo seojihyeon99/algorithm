cf6aac61d89cf017e82ecb15dd86033da5361920
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-10-21 17:25:30 +0900

    [level 2] Title: H-Index, Time: 3.26 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,34 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        // 정렬 : O(nlogn)
+        int n = citations.length; // 논문 수
         Arrays.sort(citations);
         
-        int result = 0;
-        // 이진탐색 : O(10000log10000)
-        int l = 0;
-        int r = 10000;
-        while(l <= r) {
-            int m = (l+r)/2; // h-index 후보
+        int answer = 0; // H-Index
+        for(int i=10000; i>=0; i--) {
+            int idx = Arrays.binarySearch(citations, i);
             
-            int idx = Arrays.binarySearch(citations, m);
-            int under = idx + 1; // 이하
-            int upper = citations.length - idx; // 이상
+            // 해당 숫자가 있는 경우
+            int right = n-idx; // 이상 개수
+            int left = idx+1; // 이하 개수
+            
+            // 해당 숫자가 없는 경우
             if(idx < 0) {
-                idx = -idx - 1; // 넣을 수 있는 인덱스 위치
-                under = idx;
-                upper = citations.length - under;
+                idx = -idx-1;
+                left = idx; // 이하 개수
+                right = n-idx; // 이상 개수
             }
-
-            if(upper >= m && under <= m) {
-                result = Math.max(result, m);
-                l = m+1;
-            }
-            else if(upper < m || under > m) {
-                r = m-1;
-            }
+            
+            if(left <= i && i <= right) answer = Math.max(answer, i);
         }
         
-        return result;
+        return answer;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 82.7 MB, 시간: 0.87 ms
+메모리: 74.5 MB, 시간: 3.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 09일 16:27:34
+2024년 10월 21일 17:25:26
 
 ### 문제 설명
 
